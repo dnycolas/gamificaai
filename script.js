bf96391f956d63ee8139f833887ab28e22c27f36
dnycolas <dnycolas088@gmail.com>
2024-05-14 17:22:15 -0300

    feat: Marcadores do carrosel adicionados

--- script.js
+++ script.js
@@ -32,3 +32,57 @@ window.onresize = () => {
     iconeX.style.display = "inline"
     iconeBarra.style.display = "none"
 }
+
+// Função Carrosel
+
+let slides = [
+    'primeiro-banner',
+    'segundo-banner' ,
+    'terceiro-banner'
+]
+
+
+// essa variavel pode ser usada pra dar um numero limite de slides
+// let ultimoItem = 3
+
+let slidesAtual = 0
+
+let numerSlides = slides.length
+
+let banner = document.querySelector(".banner")
+
+banner.classList.add(slides[slidesAtual])
+
+const mostraProximoSlide = () => {
+    // Remove slide anterior
+    banner.classList.remove(slides[slidesAtual])
+
+
+
+    if(slidesAtual < (numerSlides - 1)) {
+        slidesAtual++
+    } else {
+        slidesAtual = 0
+    }
+
+    // Muda a posicao da lista de slides, para mostrar o slideAtual
+    // slidesAtual++
+
+
+    // Renderiza o slideAtual
+    banner.classList.add(slides[slidesAtual])
+}
+
+const mostraSlideAnterior = () => {
+    // Remove slide anterior    
+    banner.classList.remove(slides[slidesAtual])
+
+    if(slidesAtual > 0) {
+        slidesAtual--
+    }else {
+        slidesAtual = numerSlides - 1
+    }
+
+    // Renderiza o slideAtual
+    banner.classList.add(slides[slidesAtual])
+}
