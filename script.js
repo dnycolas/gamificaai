566f512b11664ac00fa48f8ad101bf948a4d6bac
dnycolas <dnycolas088@gmail.com>
2024-05-16 17:20:14 -0300

    feat: adicionado API nos formularios

--- script.js
+++ script.js
@@ -99,24 +99,7 @@ const selecionarSlide = (indeceSlide) => {
 
 
 
-let listaCases = [
-    {
-        imagem: "https://unsplash.it/600/400?image=40",
-        descricao: "Uma empresa de tecnologia lança im desafio de gamificação onde os funcionarios devem propor e implementar ideias inovadoras."
-    },
-    {
-        imagem: "https://unsplash.it/600/400?image=39",
-        descricao: "Uma empresa de consultoria cria uma narrativa interativa de gamificação para seu programa de trinamento."
-    },
-    {
-        imagem: "https://unsplash.it/600/400?image=84",
-        descricao: "Uma empresa de vendas umplementa uma competição gamificada entre equipes que competem pelo topo do ranking"
-    },
-    {
-        imagem: "https://unsplash.it/600/400?image=96",
-        descricao: "Uma empresa de saude promove o bem bem-estar dos funcionarios atraves de um desafio de gamificação de condicionamento fisico"
-    },
-]
+let listaCases = []
 
 const renderizarCases = () => {
     let elementoLista = document.getElementById("lista-cards")
@@ -133,4 +116,48 @@ const renderizarCases = () => {
     })
 
     elementoLista.innerHTML = template
+}
+
+const carregarCases = () => {
+    fetch("http://localhost:3000/cases")
+    .then( resposta => resposta.json() )
+    .then( (dados) => {
+        listaCases = dados
+        renderizarCases()
+    } )
+    .catch( erro => console.error(erro))
+}
+
+
+
+
+
+const solicitarOrcamento = () => {
+    // Pegar valorez dos inputs
+    let valorNome = document.getElementById("campo-nome").value
+    let valorEmail = document.getElementById("campo-email").value
+    let valorDescricao = document.getElementById("campo-descricao").value
+
+    // Organizar objetos com os valores
+    let dadosForm = {
+        nome: valorNome,
+        email: valorEmail,
+        descricao: valorDescricao
+    }
+
+    // Enviar requisicao para api
+    // 127.0.0.1 -> localhost
+    // metodo HTTP POST - Create -> Cadastra post
+    fetch("http://127.0.0.1:3000/solicitacoes", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dadosForm)
+    })
+    .then (resposta => console.log(resposta))
+    .catch(erro => console.error(erro))
+        // Limpar os campos
+        // MOstrar aler com msg de sucesso
+        // CASO ERRO - alert com msg erro
 }
