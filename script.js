d10fafc9efcde03a9bf90a4bb589656ba0a44695
dnycolas <dnycolas088@gmail.com>
2024-05-16 14:29:32 -0300

    feat: implementado novos botões

--- script.js
+++ script.js
@@ -86,3 +86,51 @@ const mostraSlideAnterior = () => {
     // Renderiza o slideAtual
     banner.classList.add(slides[slidesAtual])
 }
+
+const selecionarSlide = (indeceSlide) => {
+    slides.forEach(slide => banner.classList.remove(slide))
+
+    slidesAtual = indeceSlide
+
+    banner.classList.add(slides[indeceSlide])
+}
+
+
+
+
+
+let listaCases = [
+    {
+        imagem: "https://unsplash.it/600/400?image=40",
+        descricao: "Uma empresa de tecnologia lança im desafio de gamificação onde os funcionarios devem propor e implementar ideias inovadoras."
+    },
+    {
+        imagem: "https://unsplash.it/600/400?image=39",
+        descricao: "Uma empresa de consultoria cria uma narrativa interativa de gamificação para seu programa de trinamento."
+    },
+    {
+        imagem: "https://unsplash.it/600/400?image=84",
+        descricao: "Uma empresa de vendas umplementa uma competição gamificada entre equipes que competem pelo topo do ranking"
+    },
+    {
+        imagem: "https://unsplash.it/600/400?image=96",
+        descricao: "Uma empresa de saude promove o bem bem-estar dos funcionarios atraves de um desafio de gamificação de condicionamento fisico"
+    },
+]
+
+const renderizarCases = () => {
+    let elementoLista = document.getElementById("lista-cards")
+
+    // template strings
+    let template = ""
+
+    listaCases.forEach( card => {
+            template +=  `<div class="card">
+            <img src="${card.imagem}" alt="">
+            <p>${card.descricao}</p>
+            <button>Ver mais</button>
+        </div>`
+    })
+
+    elementoLista.innerHTML = template
+}
